39358fdf280c50824534b3079a07b33e25ea1bb4
AndPotap <apotapczynski@gmail.com>
2019-11-04 20:58:56 -0500

    Copy pasted deleting the dot files

--- bin/createlink.sh
+++ bin/createlink.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 rm ~/.vimrc
+rm ~/.bashrc
+rm ~/.bash_profile
+rm ~/.bash_aliases
+rm ~/.tmux.conf
+rm ~/.ideavimrc
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.ideavimrc  ~/.ideavimrc
