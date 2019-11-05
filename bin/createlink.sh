f92ef6d4d2511dc321185c162efdcb5443e91562
AndPotap <apotapczynski@gmail.com>
2019-11-04 21:01:31 -0500

    Added the files under .config

--- bin/createlink.sh
+++ bin/createlink.sh
@@ -7,9 +7,14 @@ rm ~/.bash_aliases
 rm ~/.tmux.conf
 rm ~/.ideavimrc
 
+rm ~/.config/flake8
+rm ~/.config/nvim/init.vim
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.ideavimrc  ~/.ideavimrc
+ln -s ~/dotfiles/.config/flake8 ~/.config/flake8
+ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
