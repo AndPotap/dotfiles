39393e81a7b539c10c98564e274c6701a9aa3384
AndPotap <apotapczynski@gmail.com>
2020-09-20 11:43:02 -0500

    Added spectrwm_us keybindings

--- bin/create_robust.sh
+++ bin/create_robust.sh
@@ -2,7 +2,7 @@
 
 dirs_to_create=(vim vim/colors .config .config/nvim .config/nvim/syntax
                .config/nvim/colors bin .config/alacritty .config/kitty
-               .config/zathura .spectrwm.conf)
+               .config/zathura .spectrwm_us.conf .spectrwm.conf)
 for dir in "${dirs_to_create[@]}"
 do
     if [ ! -d ~/$dir ]
