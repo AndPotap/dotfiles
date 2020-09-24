743c2d2c9d5511612d4d35874200b6791c95efcf
AndPotap <apotapczynski@gmail.com>
2020-09-24 16:46:24 -0500

    Syncs two other files

--- bin/create_robust.sh
+++ bin/create_robust.sh
@@ -20,6 +20,8 @@ files_to_create=(.vimrc .bashrc .bash_profile .bash_aliases
                  .config/nvim/syntax/txt.vim .config/nvim/colors/gruv.vim
                  .config/kitty/kitty.conf
                  .spectrwm.conf
+                 .pylintrc
+                 .xmodmap
                  baraction.sh
                  .spectrwm_us.conf)
 for f in "${files_to_create[@]}"
