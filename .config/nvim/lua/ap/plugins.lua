c4016a326d24b62d2bdd51f7c5035e6f0f8d42d9
AndPotap <AndPotap@github.com>
2022-08-26 12:58:28 -0400

    Uses my version of the fork

--- .config/nvim/lua/ap/plugins.lua
+++ .config/nvim/lua/ap/plugins.lua
@@ -1,6 +1,7 @@
 return require('packer').startup(function(use)
     use 'jpalardy/vim-slime'
-    use 'folke/tokyonight.nvim'
+    -- use 'folke/tokyonight.nvim'
+    use 'AndPotap/tokyonight.nvim'
     use 'wbthomason/packer.nvim'
     use 'neovim/nvim-lspconfig'
     use 'williamboman/nvim-lsp-installer'
