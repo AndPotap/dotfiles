62d6c08fa7c2fc7365757b8dcc5f073be1fcf9a7
AndPotap <AndPotap@github.com>
2022-09-18 14:12:00 -0400

    Added yapf as default

--- .config/nvim/lua/ap/lsp/settings/pylsp.lua
+++ .config/nvim/lua/ap/lsp/settings/pylsp.lua
@@ -1,9 +1,16 @@
 return {
     settings = {
+        formatCommand = {"yapf"},
         pylsp = {
             plugins = {
+                autopep8 = {
+                    enabled = false
+                },
                 pycodestyle = {
                     maxLineLength = 100
+                },
+                yapf = {
+                    enabled = true
                 }
             }
         }
