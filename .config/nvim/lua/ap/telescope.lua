0a6d3469e8a8a40566ce885db4bb71e89fc8aba1
AndPotap <AndPotap@github.com>
2022-09-24 20:50:45 -0400

    Ignores jupyter notebooks

--- .config/nvim/lua/ap/telescope.lua
+++ .config/nvim/lua/ap/telescope.lua
@@ -3,6 +3,7 @@ local telescope = require("telescope")
 local actions = require("telescope.actions")
 telescope.setup {
     defaults = {
+        file_ignore_patterns = { "%.ipynb" , "%.csv" },
         mappings = {
             i = {
                 ["<CR>"] = actions.select_vertical,
