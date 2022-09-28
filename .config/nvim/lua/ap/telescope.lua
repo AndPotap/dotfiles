5fc050374562b4c4718ad6ab0cfff10fbaaa6d0b
AndPotap <AndPotap@github.com>
2022-09-27 20:26:46 -0400

    Added other extensions

--- .config/nvim/lua/ap/telescope.lua
+++ .config/nvim/lua/ap/telescope.lua
@@ -3,7 +3,7 @@ local telescope = require("telescope")
 local actions = require("telescope.actions")
 telescope.setup {
     defaults = {
-        file_ignore_patterns = { "%.ipynb" },
+        file_ignore_patterns = { "%.ipynb", "%.yaml", "%.pkl", "%.log", "%.png", "%.pyc", "%.npy", "%.pdf"},
         mappings = {
             i = {
                 ["<CR>"] = actions.select_vertical,
