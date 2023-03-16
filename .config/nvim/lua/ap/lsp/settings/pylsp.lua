23d6f3ff724a070abf8b587a9668f0a528dc66d8
AndPotap <AndPotap@github.com>
2023-03-15 21:37:07 -0400

    Fixed formatting pylsp config.

--- .config/nvim/lua/ap/lsp/settings/pylsp.lua
+++ .config/nvim/lua/ap/lsp/settings/pylsp.lua
@@ -1,9 +1,19 @@
 return {
     settings = {
+        formatCommand = { "yapf" },
         pylsp = {
             plugins = {
                 pycodestyle = {
+                    enabled = true,
                     maxLineLength = 100
+                },
+                yapf = {
+                    enabled = true
+                },
+                autopep8 = {
+                    enabled = false
                 }
             }
         }
+    }
+}
