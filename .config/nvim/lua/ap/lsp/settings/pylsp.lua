8d5204d841858501045a71edee375afdae8c5444
AndPotap <AndPotap@github.com>
2023-04-13 15:36:49 -0400

    Made similar to desk alt

--- .config/nvim/lua/ap/lsp/settings/pylsp.lua
+++ .config/nvim/lua/ap/lsp/settings/pylsp.lua
@@ -1,9 +1,17 @@
 return {
     settings = {
+        formatCommand = {"yapf"},
         pylsp = {
             plugins = {
+                autopep8 = {
+                    enabled = false
+                },
                 pycodestyle = {
+                    enabled = false,
                     maxLineLength = 100
+                },
+                yapf = {
+                    enabled = true
                 }
             }
         }
