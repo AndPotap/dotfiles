8eddca2d10076b9cda3c3f33c80d507780aa98c7
AndPotap <apotapczynski@gmail.com>
2023-10-09 12:30:42 -0400

    Added proper spacing (should I add txt to the list?)

--- .config/nvim/lua/ap/tree.lua
+++ .config/nvim/lua/ap/tree.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup {
     ignore_install = { "c" },
     highlight = {
         enable = true,
-        disable = { "markdown", "vim", "latex", "gpg"},
+        disable = { "markdown", "vim", "latex", "gpg" },
         additional_vim_regex_highlighting = false,
     },
 }
