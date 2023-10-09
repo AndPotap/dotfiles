993347d40845a04307cb217fe3c564050b460f2f
AndPotap <apotapczynski@gmail.com>
2023-10-09 08:54:36 -0400

    Added text group and syntax (but failing to load automatically)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,13 +10,13 @@ require('ap.slime')
 
 vim.cmd.colorscheme("tokyonight")
 
-local grp = vim.api.nvim_create_augroup("pythonops", { clear = true })
 local autocmd = vim.api.nvim_create_autocmd
 
 local function noremap(k, v)
     vim.keymap.set('n', k, v, { silent = true, noremap = true })
 end
 
+local grp_python = vim.api.nvim_create_augroup("pythonops", { clear = true })
 local function enable_python_keys()
     noremap('<leader>b', 'A<Enter>breakpoint()<Esc>')
     noremap('<leader>ff', '/def<Space><Enter>')
@@ -27,5 +27,20 @@ end
 
 autocmd("FileType", { pattern = { "python" },
     callback = enable_python_keys,
-    group = grp
+    group = grp_python
+})
+
+local grp_text = vim.api.nvim_create_augroup("textops", { clear = true })
+local function enable_text_keys()
+    noremap('<leader>0', '%s/\\s\\+$//e')
+    noremap('<leader>cl', '/%%%%<cr>')
+    noremap('<leader>ff', '/##<cr>')
+    vim.o.spell = true
+    -- vim.opt.syntax = "txt"
+    -- vim.cmd([[set syntax=txt]])
+end
+
+autocmd("FileType", { pattern = { "text" },
+    callback = enable_text_keys,
+    group = grp_text
 })
