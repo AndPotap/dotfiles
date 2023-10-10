45f39bf0fdabec4434d8f2d9ab036338fdc97c50
AndPotap <apotapczynski@gmail.com>
2023-10-09 20:39:53 -0400

    Added the autocomplete support

--- .config/nvim/lua/ap/packer.lua
+++ .config/nvim/lua/ap/packer.lua
@@ -1,4 +1,11 @@
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use "neovim/nvim-lspconfig"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-path"
+  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use { "tzachar/cmp-fuzzy-buffer", requires = { "tzachar/fuzzy.nvim" } }
+  use "saadparwaiz1/cmp_luasnip"
+  use "L3MON4D3/LuaSnip"
 end)
