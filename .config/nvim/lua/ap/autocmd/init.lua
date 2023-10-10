93a7ec7d6416fbff05955ab2e91efcbc7917e706
AndPotap <apotapczynski@gmail.com>
2023-10-09 20:30:18 -0400

    Started with maps only

--- .config/nvim/lua/ap/autocmd/init.lua
+++
@@ -1,4 +0,0 @@
-require("ap.autocmd.gpg")
-require("ap.autocmd.python")
-require("ap.autocmd.text")
-require("ap.autocmd.latex")
--- .config/nvim/lua/ap/slime.lua
+++
@@ -1,8 +0,0 @@
-vim.g.slime_target = "tmux"
-vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
-vim.g.slime_paste_file = vim.fn.expand("$HOME/.slime_paste")
-vim.g.slime_dont_ask_default = true
-vim.g.slime_python_ipython = true
-vim.api.nvim_set_keymap("n", "<C-i>", "<Plug>SlimeLineSend", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-q>", "<Plug>SlimeParagraphSend", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<C-i>", "<Plug>SlimeRegionSend", { noremap = true, silent = true })
