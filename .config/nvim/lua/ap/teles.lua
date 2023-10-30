4eb759347dd98b9057b46dbade446854d2073042
AndPotap <apotapczynski@gmail.com>
2023-10-30 13:04:49 -0400

    Added cola as a default

--- .config/nvim/lua/ap/teles.lua
+++ .config/nvim/lua/ap/teles.lua
@@ -24,6 +24,20 @@ telescope.setup {
 }
 telescope.load_extension("fzf")
 
+search_specific_dir = function()
+    builtin.find_files({
+        prompt_title = "< CoLA >",
+        cwd = "~/cola",
+        -- attach_mappings = function(_, map)
+        --     map("i", "<CR>", actions.select_vertical + actions.center)
+        --     map("n", "<CR>", actions.select_vertical + actions.center)
+        --     return true
+        -- end
+    })
+end
+
+vim.keymap.set("n", "<leader>fc", search_specific_dir, { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>g", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
