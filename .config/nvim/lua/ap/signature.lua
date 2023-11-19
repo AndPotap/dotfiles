1c79df01be693b48c1df90b25c377e1861580932
Andres Potapczynski Guiza <ap6604@log-3.hpc.nyu.edu>
2023-11-18 21:01:32 -0500

    Finished other merging

--- .config/nvim/lua/ap/signature.lua
+++ .config/nvim/lua/ap/signature.lua
@@ -3,7 +3,6 @@ local lsp_signature = require("lsp_signature")
 local signature_config = {
     log_path = vim.fn.expand("$HOME") .. "/tmp/sig.log",
     debug = false,
-    doc_lines=0,
     zindex = 200,
     hint_enable = false,
     handler_opts = { border = "single" },
