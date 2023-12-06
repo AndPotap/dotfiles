75d02d0356301d0acc98d4ff02689165d37cf77a
AndPotap <apotapczynski@gmail.com>
2023-12-06 12:44:01 -0500

    Added

--- .config/nvim/lua/ap/bib_maps.lua
+++ .config/nvim/lua/ap/bib_maps.lua
@@ -2,8 +2,11 @@ local M = {}
 
 M.setup_keymaps = function ()
     vim.fn.setreg("/", "+++")
-    vim.fn.setreg("n", "Advances in Neural Information Processing Systems (NeurIPS)")
-    vim.fn.setreg("i", "International Conference on Machine Learning (ICML)")
+    vim.fn.setreg("1", "Advances in Neural Information Processing Systems (NeurIPS)")
+    vim.fn.setreg("2", "International Conference on Machine Learning (ICML)")
+    vim.fn.setreg("3", "International Conference on Learning Representations (ICLR)")
+    vim.fn.setreg("4", "International Conference on Artificial Intelligence and Statistics (AISTATS)")
+    vim.fn.setreg("5", "Association for the Advancement of Artificial Intelligence (AAAI)")
 end
 
 return M
