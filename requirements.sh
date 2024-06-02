da069427fa811ebc5389af588bcc425dea4c50db
Andres Potapczynski <andpotap@amazon.com>
2024-06-02 12:00:20 +0000

    Added ec2 requirements

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+python3 -m pip install --upgrade pip
+# python3 -m pip install torch torchvision torchaudio --no-cache-dir
+python3 -m pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --no-cache-dir
+python3 -m pip install pytest numpy pandas matplotlib pyarrow dataclasses-json psutil boto3 tensorboard lightning --no-cache-dir
+python3 -m pip install ~/gpustat/.
+python3 -m pip install "python-lsp-server[all]" --no-cache-dir
