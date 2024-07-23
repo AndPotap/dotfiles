34fbda87305260e174fd97190b18677daef52713
AndPotap <apotapczynski@gmail.com>
2024-07-23 01:50:56 +0000

    Updated

--- .bash_profile
+++ .bash_profile
@@ -13,14 +13,18 @@ random_number=$((RANDOM % 8))
 sGPU $random_number
 
 export DEEP_TS_PATH="$HOME/efs/andpotap/"
+export CUDA_HOME="/usr/local/cuda-11.8"
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${CUDA_HOME}/lib
+export GOPATH=${HOME}/go
+export PATH=/usr/bin/
+export PATH=$PATH:${CUDA_HOME}/bin
 export PATH=$PATH:"$HOME/bin"
 export PATH=$PATH:"$HOME/.local/bin"
+export PATH=$PATH:"$HOME/.fzf/bin"
 export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
 export PATH=$PATH:/usr/local/go/bin
-export GOPATH=${HOME}/go
 export PYTHONSTARTUP="$(python3 -m jedi repl)"
 export PYTHONPATH=""
 export PYTHONPATH=$PYTHONPATH:"$HOME/cola"
 export PYTHONPATH=$PYTHONPATH:"$HOME/trainkit"
 export PYTHONPATH=$PYTHONPATH:"$HOME/efs/andpotap/ForecastingDeepTS/python"
-# export PYTHONPATH=$PYTHONPATH:"$HOME/neuralforecast"
--- requirements.sh
+++ requirements.sh
@@ -1,14 +1,15 @@
 python3 -m pip install --upgrade pip
-python3 -m pip install torch torchvision torchaudio --no-cache-dir
+# python3 -m pip install torch torchvision torchaudio --no-cache-dir
 # python3 -m pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --no-cache-dir
-# python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-python3 -m pip install neuralforecast matplotlib jupyter
+# python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 --no-cache-dir
+python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+python3 -m pip install neuralforecast matplotlib jupyter --no-cache-dir
 python3 -m pip install pytest pyarrow pandas numpy matplotlib dataclasses-json psutil boto3 tensorboard lightning --no-cache-dir
 python3 -m pip install docker --no-cache-dir
 python3 -m pip install ~/gpustat/.
-python3 -m pip install "python-lsp-server[all]" --no-cache-dir
 python3 -m pip install ruff --no-cache-dir
-python3 -m pip uninstall autopep8 -y
 python3 -m pip install python-lsp-ruff --no-cache-dir
+# python3 -m pip install "python-lsp-server[all]" --no-cache-dir
+# python3 -m pip uninstall autopep8 -y
 # python3 -m pip install python-lsp-black --no-cache-dir
 # python3 -m pip install python-lsp-isort --no-cache-dir
