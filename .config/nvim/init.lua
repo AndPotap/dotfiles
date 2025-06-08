49aeaa785770d6d399d48e8ff94724e72dd88878
AndPotap <apotapczynski@gmail.com>
2025-06-08 17:42:50 -0400

    Fixed the non appearing messages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,3 +8,10 @@ require("ap.status")
 require("ap.signature")
 require("ap.slime")
 require("ap.snippets")
+
+vim.diagnostic.config({
+  virtual_text = true,  -- Inline error text
+  signs = true,         -- Gutter signs
+  underline = true,     -- Underline errors
+  update_in_insert = false,
+})
