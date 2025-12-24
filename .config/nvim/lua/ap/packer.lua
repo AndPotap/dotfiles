a6da554db9deb8dac86f4862c7dd4ff0e7286abf
AndPotap <apotapczynski@gmail.com>
2025-12-24 10:53:07 -0500

    Took out markdown preview

--- .config/nvim/lua/ap/packer.lua
+++ .config/nvim/lua/ap/packer.lua
@@ -21,9 +21,9 @@ return require("packer").startup(function(use)
   use "jpalardy/vim-slime"
   -- use "mbbill/undotree"
   -- use "Vigemus/iron.nvim"
-  use({ "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-        ft = { "markdown" },
-    })
+  -- use({ "iamcco/markdown-preview.nvim",
+  --       run = "cd app && npm install",
+  --       setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  --       ft = { "markdown" },
+  --   })
 end)
