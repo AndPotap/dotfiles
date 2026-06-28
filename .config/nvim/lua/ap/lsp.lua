dbfcba7ceb1b8124cf4e806dec229c01392450b5
AndPotap <apotapczynski@gmail.com>
2026-06-28 07:51:10 -0400

    Added mojo formatting as a command

--- .config/nvim/lua/ap/lsp.lua
+++ .config/nvim/lua/ap/lsp.lua
@@ -1,9 +1,16 @@
--- vim.lsp.set_log_level("debug")
--- Enable above to see logs from :LspLog
-
 vim.lsp.config("clangd", { cmd = { "clangd-12" } })
--- vim.lsp.config("mojo", { cmd = {"mojo-lsp-server"}, filetypes = { "mojo" } })
 vim.lsp.config("rust_analyzer", {})
+vim.lsp.config("mojo", { cmd = { "mojo-lsp-server" }, filetypes = { "mojo" } })
+
+local function format_fn()
+  if vim.bo.filetype ~= "mojo" then
+    vim.lsp.buf.format({ async = true })
+    return
+  end
+
+  vim.fn.system({ "mojo", "format", vim.fn.expand("%") })
+  vim.cmd("silent e!")
+end
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -17,6 +24,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
     vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<space>au", function() vim.lsp.buf.format { async = true } end, opts)
+    -- vim.keymap.set("n", "<space>au", function() vim.lsp.buf.format { async = true } end, opts)
+    vim.keymap.set("n", "<space>au", format_fn, opts)
   end,
 })
